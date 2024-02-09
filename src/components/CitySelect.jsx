5e13c4d75ee15e56fbe3483c450cfe218869f34d
robertastira <stira.roberta@yahoo.it>
2024-02-09 14:51:53 +0100

    New Commit

--- src/components/CitySelect.jsx
+++ src/components/CitySelect.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 import Form from 'react-bootstrap/Form'
 
 
@@ -6,19 +6,23 @@ import Form from 'react-bootstrap/Form'
 const CitySelect = (props) => {
 
   return (
-
+<Container>
+  <Row>
+    <Col></Col>
     <Form.Select
       aria-label="cityname"
       className=''
       value={props.cityName}
-      onChange={(e) => props.changeCityName(e.target.value)}
-    >
+      onChange={(e) => props.changeCityName(e.target.value)}>
       <option>Milano</option>
       <option>Palermo</option>
       <option>Parma</option>
       <option>Altamura</option>
       <option>Roma</option>
     </Form.Select>
+    <Col></Col>
+    </Row>
+    </Container>
   )
 }
 
